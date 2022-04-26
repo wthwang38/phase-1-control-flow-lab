2a175cc70e4cbb419411c196398e6901b4c42241
William Hwang <willhwang90@gmail.com>
2022-04-25 21:30:18 -0400

    commit tree f94c97e3f887673aebf0d423b1a7be2c653d64fc

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result
+  if(feet <= 400){
+    return result = "This one is on me!"
+  } else if(feet <= 2000){
+    return result = "That will be twenty bucks."
+  } else if(feet <= 2500){
+    return result = "I will gladly take your thirty bucks."
+  } else if(feet >= 2501){ 
+    return result = "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const cityCheck = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+  return cityCheck;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let greeting;
+  switch (tip){
+    case 'generous':
+      greeting = "Thank you so much.";
+      break;
+    case 'not as generous':
+      greeting = "Thank you.";
+      break;
+    default:
+      greeting = "Bye.";
+      break;
+
+  }
+  return greeting;
 }
